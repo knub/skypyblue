6e6706f19e243cbfc8603b461efbc16c152a6294
Dimitri Korsch <korschdima@yahoo.de>
2014-05-06 09:13:27 +0200

    added some test examples and a testrunner

---
+++ tests/sometestmodule/__init__.py
@@ -0,0 +1,1 @@
+from sometest import *
---
+++ tests/testrunner.py
@@ -0,0 +1,16 @@
+"""
+Usage: 
+  ./testrunner.py                     -> executes all tests
+  ./testrunner.py <TestCaseClass>     -> executes only tests from this testcase
+"""
+
+if __name__ != "__main__": exit()
+
+import unittest, sys
+sys.path.append("../src")
+
+
+from sometestmodule import *
+
+unittest.main()
+
