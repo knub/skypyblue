1fd9f2c2c0a9416744da24036da5ab47cabca18f
Dimitri Korsch <korschdima@yahoo.de>
2014-05-07 12:34:59 +0200

    renamed assertEquals to assertEqual, because it was deprecated

--- tests/sometestmodule/sometest.py
+++ tests/sometestmodule/sometest.py
@@ -3,10 +3,10 @@ from somemodule import some_func, some_other_func
 
 class SomeTestClass(unittest.TestCase):
   def test_the_function(self):
-    self.assertEquals(some_func(3), 4)
+    self.assertEqual(some_func(3), 4)
 
 class SomeOtherTestClass(unittest.TestCase):
   def test_the_other_function(self):
-    self.assertEquals(some_other_func(4), 2)
+    self.assertEqual(some_other_func(4), 2)
     with self.assertRaises(ValueError):
       some_other_func(-4)
