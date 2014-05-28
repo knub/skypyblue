16c7c20e9266c365db98dc35783fa6582e3307b9
Stefan Bunk <stefan.bunk@googlemail.com>
2014-05-28 11:42:05 +0200

    Moved interface example to examples.

--- examples/interface_example.py
+++ examples/interface_example.py
@@ -17,7 +17,7 @@ m12_34 = Method([v1, v2], [v3, v4],
   lambda v1, v2: (v1 + v2 , v1 - v2))
 
 m34_12 = Method([v3, v4], [v1, v2],
-  lambda v3, v4: (v3 + v4) / 2 , (v3 - v4) / 2))
+  lambda v3, v4: (v3 + v4) / 2 , (v3 - v4) / 2)
 
 cn1 = Constraint(
   lambda v1, v2, v3, v4: v1 + v2 == v3 and v1 - v2 == v4,
