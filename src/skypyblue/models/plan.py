afd3002b0ccfe095cc9ee64dcd539801ffc3d5c2
Daniel Kurzynski <daniel.kurzynski@student.hpi.uni-potsdam.de>
2014-06-18 12:37:02 +0200

    plan class

---
+++ src/skypyblue/models/plan.py
@@ -0,0 +1,15 @@
+class Plan:
+  def __init__(self, root_constraints, good_constraints, bad_constraints, valid):    
+    """
+    root_constraints \tconstraints used to construct this plan
+    good_constraints \t ordered list of enforced constraints in plan
+    bad_constraints \t enforced constraints in and downstream of cycles
+    valid:\ttrue if this plan is valid
+    """
+    self.root_constraints = root_constraints
+    self.good_constraints = good_constraints
+    self.bad_constraints = bad_constraints
+    self.valid = valid
+
+    for constraint in set(self.root_constraints+self.good_constraints+self.bad_constraints):
+      constraint.add_valid_plan(self)
