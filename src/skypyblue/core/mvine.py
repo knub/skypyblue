f99d41d0d03d2ced573337c560eedecc5557d25c
Dimitri Korsch <korschdima@yahoo.de>
2014-06-18 09:32:17 +0200

    redetermined_vars is instance variable of mvine now

--- src/skypyblue/core/mvine.py
+++ src/skypyblue/core/mvine.py
@@ -5,6 +5,7 @@ class Mvine:
     self.marker = marker
     self.stack = []
     self.root_strength = Strength.WEAKEST
+    self.redetermined_vars = []
 
   @property
   def mark(self):
@@ -14,35 +15,36 @@ class Mvine:
     self.marker.new_mark()
     self.stack = []
     self.root_strength = cn.strength
-    return self.enforce_cn(cn, redetermined_vars)
+    self.redetermined_vars = redetermined_vars
+    return self.enforce_cn(cn)
 
-  def grow(self, redetermined_vars):
+  def grow(self):
     if not self.stack: return True
     cn = self.stack.pop()
     if cn.mark == self.mark:
-      ok = self.grow(redetermined_vars)
+      ok = self.grow()
     elif Strength.weaker(cn.strength, self.root_strength):
-      ok = self.revoke_cn(cn, redetermined_vars)
+      ok = self.revoke_cn(cn)
     else:
-      ok = self.enforce_cn(cn, redetermined_vars)
+      ok = self.enforce_cn(cn)
     if not ok: self.stack.append(cn)
     return ok
 
-  def revoke_cn(self, cn, redetermined_vars):
+  def revoke_cn(self, cn):
     cn.mark = self.mark
-    if self.grow(redetermined_vars):
+    if self.grow():
       for var in cn.selected_method.outputs:
         if var.mark != self.mark:
           var.determined_by = None
           var.walk_strength = Strength.WEAKEST
-          redetermined_vars.append(var)
+          self.redetermined_vars.append(var)
       cn.selected_method = None
       return True
     else:
       cn.mark = None
       return False
 
-  def enforce_cn(self, cn, redetermined_vars):
+  def enforce_cn(self, cn):
     cn.mark = self.mark
     for mt in cn.methods:
       if self.is_possible_method(mt, cn):
@@ -51,17 +53,17 @@ class Mvine:
           self.stack.append(new_cn)
         for var in mt.outputs:
           var.mark = self.mark
-        if self.grow(redetermined_vars):
+        if self.grow():
           if not cn.selected_method is None:
             for var in cn.selected_method.outputs:
               if var.mark != self.mark:
                 var.determined_by = None
                 var.walk_strength = Strength.WEAKEST
-                redetermined_vars.append(var)
+                self.redetermined_vars.append(var)
           cn.selected_method = mt
           for var in mt.outputs:
             var.determined_by = cn
-            redetermined_vars.append(var)
+            self.redetermined_vars.append(var)
           return True
         else:
           for var in mt.outputs: var.mark = None
--- tests/mvine_tests.py
+++ tests/mvine_tests.py
@@ -23,15 +23,13 @@ class MVineTests(TestCase):
     cn.mark = new_mark()
 
     new_mark()
-    self.mvine.stack = []
-    self.root_strength = Strength.WEAKEST
-    not_revoked = self.mvine.revoke_cn(cn, [])
+    not_revoked = self.mvine.revoke_cn(cn)
 
     self.assertFalse(not_revoked)
     self.assertTrue(cn.mark is None)
 
-  def test_mvine_revoke_cn_succeeds(self):
-    self.cs.mvine_grow = Mock(return_value = True)
+  def test_mvine_revoke_cn_succeeds_case1(self):
+    self.mvine.grow = Mock(return_value = True)
 
     cn = Constraint(None, Strength.WEAKEST, [], [])
     v1 = self.cs.create_variable("v1", 1)
@@ -42,47 +40,51 @@ class MVineTests(TestCase):
     m = Method([v1, v2], v3, None)
     cn.selected_method = m
     v3.determined_by = m
-    redetermined_vars = []
 
     new_mark()
-    self.mvine.stack = []
-    self.mvine.root_strength = Strength.WEAKEST
-    not_revoked = self.mvine.revoke_cn(cn, redetermined_vars)
+    not_revoked = self.mvine.revoke_cn(cn)
 
     self.assertTrue (not_revoked)
     self.assertEqual(Strength.WEAK, v1.walk_strength)
     self.assertEqual(Strength.WEAK, v2.walk_strength)
     self.assertEqual(Strength.WEAKEST, v3.walk_strength)
     self.assertTrue (v3.determined_by is None)
-    self.assertTrue (v3 in redetermined_vars)
+    self.assertTrue (v3 in self.mvine.redetermined_vars)
     self.assertTrue (cn.selected_method is None)
 
+  def test_mvine_revoke_cn_succeeds_case2(self):
     # case 2 - v3's mark is the same as passed to mvine_revoke_cn
+    self.mvine.grow = Mock(return_value = True)
+
+    cn = Constraint(None, Strength.WEAKEST, [], [])
+    v1 = self.cs.create_variable("v1", 1)
+    v2 = self.cs.create_variable("v2", 2)
+    v3 = self.cs.create_variable("v3", 3)
+    for v in [v1,v2,v3]: v.walk_strength = Strength.WEAK
+
+    m = Method([v1, v2], v3, None)
+    cn.selected_method = m
+    v3.determined_by = m
 
     mark = new_mark()
     cn.selected_method = m
     v3.walk_strength = Strength.WEAK
     v3.determined_by = m
     v3.mark = mark
-    redetermined_vars = []
-    self.mvine.stack = []
-    self.mvine.root_strength = Strength.WEAKEST
-    not_revoked = self.mvine.revoke_cn(cn, redetermined_vars)
+    not_revoked = self.mvine.revoke_cn(cn)
 
     self.assertTrue (not_revoked)
     self.assertEqual(Strength.WEAK, v1.walk_strength)
     self.assertEqual(Strength.WEAK, v2.walk_strength)
     self.assertEqual(Strength.WEAK, v3.walk_strength)
     self.assertTrue (v3.determined_by is m)
-    self.assertTrue (v3 not in redetermined_vars)
+    self.assertTrue (v3 not in self.mvine.redetermined_vars)
     self.assertTrue (cn.selected_method is None)
 
   # mvine_revoke_cn(cn, Strength.WEAKEST, new_mark(), [], [])
   def test_mvine_grow_with_empty_stack(self):
     new_mark()
-    self.mvine.stack = []
-    self.mvine.root_strength = Strength.WEAKEST
-    self.assertTrue(self.mvine.grow([]))
+    self.assertTrue(self.mvine.grow())
 
   def test_mvine_grow_with_marked_constraints(self):
     cn1 = Constraint(None, Strength.WEAKEST, [], [])
@@ -98,7 +100,8 @@ class MVineTests(TestCase):
     self.mvine.revoke_cn = Mock(return_value = False)
     self.mvine.stack = [cn2, cn1]
     self.mvine.root_strength = Strength.MEDIUM
-    self.assertFalse(self.mvine.grow([]))
+    # self.mvine.redetermined_vars = []
+    self.assertFalse(self.mvine.grow())
 
   def test_mvine_grow_with_(self):
     cn1 = Constraint(None, Strength.WEAKEST, [], [])
@@ -107,7 +110,8 @@ class MVineTests(TestCase):
     self.mvine.enforce_cn = Mock(return_value = False)
     self.mvine.stack = [cn2, cn1]
     self.mvine.root_strength = Strength.MEDIUM
-    self.assertFalse(self.mvine.grow([]))
+    # self.mvine.redetermined_vars = []
+    self.assertFalse(self.mvine.grow())
 
 
   def test_mvine_enforce_cn_with_no_methods_fails(self):
@@ -115,9 +119,7 @@ class MVineTests(TestCase):
     cn.mark = new_mark()
     self.mvine.possible_method = Mock(return_value = False)
     new_mark()
-    self.mvine.stack = []
-    self.mvine.root_strength = Strength.WEAKEST
-    self.assertFalse(self.mvine.enforce_cn(cn, []))
+    self.assertFalse(self.mvine.enforce_cn(cn))
     self.assertTrue(cn.mark is None)
 
   def test_mvine_enforce_cn_fails(self):
@@ -129,9 +131,7 @@ class MVineTests(TestCase):
     cn.mark = new_mark()
     self.mvine.possible_method = Mock(return_value = False)
     new_mark()
-    self.mvine.stack = []
-    self.mvine.root_strength = Strength.WEAKEST
-    self.assertFalse(self.mvine.enforce_cn(cn, []))
+    self.assertFalse(self.mvine.enforce_cn(cn))
     self.assertTrue(cn.mark is None)
 
 
