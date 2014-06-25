1786564008d149dc04ca24f16c7a073214cb3532
Stefan Bunk <stefan.bunk@googlemail.com>
2014-06-26 00:41:19 +0200

    Added excludes for pycallgraph.

--- tests/performance/callgraph.py
+++ tests/performance/callgraph.py
@@ -8,11 +8,24 @@ from pycallgraph.output import GraphvizOutput
 from skypyblue_test import *
 
 config = Config()
-config.trace_filter = GlobbingFilter(include=[
-    'skypyblue.*'
+config.trace_filter = GlobbingFilter(exclude = [
+  '*listcomp*',
+  '*lambda*',
+  '*max_out*',
+  '*enum*',
+  '*skypyblue_test*',
+  '*weaker*',
+  '*__init__*',
+  '*equality_constraint*',
+  '*scale_constraint*',
+  '*has_invalid_vars*',
+  '*_check_constraints*',
+  '*_new_mark*',
+  '*pycallgraph*'
 ])
+config.groups = True
 
 graphviz = GraphvizOutput()
 
-with PyCallGraph(output=graphviz, config=config):
+with PyCallGraph(output = graphviz, config = config):
   skypyblue(1)
