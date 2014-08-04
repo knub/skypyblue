56d53dcb71a0b3e6431da898bca65dd438d4ed00
Dimitri Korsch <korschdima@yahoo.de>
2014-08-04 11:11:18 +0200

    first release ready version

--- src/setup.py
+++ src/setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 setup(
   name='skypyblue',
-  version='0.1.0',
+  version='1.0.0',
   packages=['skypyblue', 'skypyblue.core', 'skypyblue.models'],
   install_requires=['enum34'],
   test_requires=['mock']
